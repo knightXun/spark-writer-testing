c92c0f2e78ddda60c97ab4099f7868894cd1256f
knightXun <1004815462@qq.com>
2020-03-25 10:32:27 +0800

    fix filename bug

--- spark-writer/run.sh
+++ spark-writer/run.sh
@@ -11,7 +11,7 @@ echo $port
 sed -i "" "s/HOST/${host}/" spark.conf
 sed -i "" "s/PORT/${port}/" spark.conf 
 
-bin/spark-submit \
+/opt/spark-1.5.1-bin-hadoop2.6/bin/spark-submit \
  --class com.vesoft.nebula.tools.generator.v2.SparkClientGenerator \
  --master ${master_url} \
  /home/sst.generator-1.0.0-beta.jar -c conf/test.conf -h -d
