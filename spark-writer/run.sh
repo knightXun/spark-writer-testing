c05fbe4fdddc69c642153eab2f4258b907ea6fef
knightXun <1004815462@qq.com>
2020-03-25 13:32:05 +0800

    sss

--- spark-writer/run.sh
+++ spark-writer/run.sh
@@ -6,8 +6,8 @@ port=$2
 echo $host 
 echo $port
 
-sed -i  "s/HOST/${host}/" spark.conf
-sed -i  "s/PORT/${port}/" spark.conf 
+sed -i  "s/HOST/${host}/" /home/spark.conf
+sed -i  "s/PORT/${port}/" /home/spark.conf 
 
 /opt/spark-1.5.1-bin-hadoop2.6/bin/spark-submit \
  --class com.vesoft.nebula.tools.generator.v2.SparkClientGenerator \
