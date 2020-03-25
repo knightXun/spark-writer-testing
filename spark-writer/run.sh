5018fe8791585fc3dd0f52cf84dd1a924a72e188
knightXun <1004815462@qq.com>
2020-03-25 13:29:26 +0800

    sss

--- spark-writer/run.sh
+++ spark-writer/run.sh
@@ -12,6 +12,6 @@ sed -i  "s/PORT/${port}/" spark.conf
 /opt/spark-1.5.1-bin-hadoop2.6/bin/spark-submit \
  --class com.vesoft.nebula.tools.generator.v2.SparkClientGenerator \
  --master spark://spark-master:7077 \
- /home/sst.generator-1.0.0-beta.jar -c /home/spark.conf -h -d
+ /home/sst.generator.jar -c /home/spark.conf -h -d
 
 
