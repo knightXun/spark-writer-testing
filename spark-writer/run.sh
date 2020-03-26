a83b71add394a4f7727d9e66bb694d4ec431aac1
knightXun <1004815462@qq.com>
2020-03-26 15:36:42 +0800

    add sst jar

--- spark-writer/run.sh
+++ spark-writer/run.sh
@@ -9,9 +9,9 @@ echo $port
 sed -i  "s/HOST/${host}/" /home/spark.conf
 sed -i  "s/PORT/${port}/" /home/spark.conf 
 
-/opt/spark-1.5.1-bin-hadoop2.6/bin/spark-submit \
+/opt/bitnami/spark/bin/spark-submit \
  --class com.vesoft.nebula.tools.generator.v2.SparkClientGenerator \
- --master spark://spark-master:7077 \
+ --master spark://spark-master-svc:7077 \
  /home/sst.generator.jar -c /home/spark.conf -h -d
 
 
